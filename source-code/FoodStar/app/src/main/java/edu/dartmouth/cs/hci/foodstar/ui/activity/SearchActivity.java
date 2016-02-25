2bf8024421fb6d3cae5cfc05f4112a4a309d1f16
Vishal Gaurav <vishal.gaurav@hotmail.com>
2016-02-24 23:09:01 -0500

    added search filters

--- source-code/FoodStar/app/src/main/java/edu/dartmouth/cs/hci/foodstar/ui/activity/SearchActivity.java
+++ source-code/FoodStar/app/src/main/java/edu/dartmouth/cs/hci/foodstar/ui/activity/SearchActivity.java
@@ -7,11 +7,13 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -30,6 +32,20 @@ public class SearchActivity extends AppCompatActivity {
     private List<Recipe> mListRecipes = new ArrayList<>();
     private FoodAdapter mAdapter = null;
     private EditText mEdtSearch;
+    private Button mBtnApplyFilter;
+    private EditText mEdtProteinMax;
+    private EditText mEdtProteinMin;
+    private EditText mEdtFatMax;
+    private EditText mEdtFatMin;
+    private EditText mEdtCarbMin;
+    private EditText mEdtCarbMax;
+    private EditText mEdtTimeMin;
+    private EditText mEdtTimeMax;
+    private EditText mEdtRating;
+    private Button mBtnApply;
+    DrawerLayout mDrawer;
+    private Button mBtnClear;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +53,10 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.setDrawerListener(toggle);
         toggle.syncState();
         mGvSearchResults = (GridView) findViewById(R.id.gvSearchResults);
         mGvSearchResults.setEmptyView(findViewById(R.id.txtEmptyView));
@@ -48,10 +64,28 @@ public class SearchActivity extends AppCompatActivity {
         mGvSearchResults.setAdapter(mAdapter);
         mGvSearchResults.setOnItemClickListener(onItemClickListener);
         mEdtSearch = (EditText) findViewById(R.id.edtSearch);
+        mBtnApplyFilter = (Button) findViewById(R.id.btnApply);
+        mBtnClear = (Button) findViewById(R.id.btnClear);
+        mBtnApplyFilter.setOnClickListener(mOnClickListener);
+        mBtnClear.setOnClickListener(mOnClickListener);
         mEdtSearch.setOnEditorActionListener(mEditorActionListener);
         //addHardCodeData();
         mAdapter.notifyDataSetChanged();
         mEdtSearch.setShowSoftInputOnFocus(true);
+
+        mEdtProteinMax = (EditText) findViewById(R.id.edtProteinMax);
+        mEdtProteinMin = (EditText) findViewById(R.id.edtProteinMin);
+        mEdtFatMin = (EditText) findViewById(R.id.edtFatMax);
+        mEdtFatMax = (EditText) findViewById(R.id.edtFatMin);
+        mEdtCarbMax = (EditText) findViewById(R.id.edtCarbMax);
+        mEdtCarbMin = (EditText) findViewById(R.id.edtCarbMin);
+        mEdtTimeMin = (EditText) findViewById(R.id.edtTimeMin);
+        mEdtTimeMax = (EditText) findViewById(R.id.edtTimeMax);
+        mEdtRating = (EditText) findViewById(R.id.edtRating);
+        mBtnApply = (Button) findViewById(R.id.btnApply);
+        mBtnClear = (Button) findViewById(R.id.btnClear);
+
+
     }
 
     private void addHardCodeData() {
@@ -91,8 +125,41 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        // getMenuInflater().inflate(R.menu.search, menu);
-        return false;
+        getMenuInflater().inflate(R.menu.search, menu);
+        return true;
+    }
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnApply: {
+                    onClickFilterApply();
+                }
+                break;
+                case R.id.btnClear: {
+                    onClickFilterClear();
+                }
+                break;
+            }
+        }
+    };
+
+    private void onClickFilterApply() {
+        mDrawer.closeDrawer(Gravity.LEFT);
+    }
+
+    private void onClickFilterClear() {
+        mEdtRating.setText("");
+        mEdtTimeMax.setText("");
+        mEdtTimeMin.setText("");
+        mEdtProteinMax.setText("");
+        mEdtProteinMin.setText("");
+        mEdtFatMax.setText("");
+        mEdtFatMin.setText("");
+        mEdtCarbMin.setText("");
+        mEdtCarbMax.setText("");
+
     }
 
     private TextView.OnEditorActionListener mEditorActionListener = new TextView.OnEditorActionListener() {
@@ -125,7 +192,8 @@ public class SearchActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_filter) {
+            mDrawer.openDrawer(Gravity.LEFT);
             return true;
         }
 
